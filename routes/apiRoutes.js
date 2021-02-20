ddbe5b8d8e97580b4bb9e26590cbd85cc88dab24
emilykidd3 <e.kidd61@yahoo.com>
2021-02-19 20:37:00 -0800

    bug fix to have cusstyles.css load for /notes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+// get data from db.json & parse
+router.get('/notes', (req, res) => {
+    fs.readFile ('db/db.json', 'UTF-8', (err, data) => {
+        if (err) {
+            throw err
+        }
+        const notes = JSON.parse(data);
+        res.json(notes);
+    })
+});
+
+router.post('/notes', (req, res) => {
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,5 +3,21 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+// makes it so you dont have to put public in the route??  
+app.use(express.static('public'));
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
